8a0dff054c0e267283b2a0d93c170b4014fc39e0
Benedettopana <benedetto.pana@gmail.com>
2024-03-11 17:15:03 +0100

    [add]street/[fix]animation

--- js/script.js
+++ js/script.js
@@ -7,17 +7,39 @@ createApp({
       myImg: '',
       msgAnimation: 'Fai click sul personaggio!',
       immagine: '<img src="./assets/img/2151120916.jpg" alt="landscape">',
-      pgW: '<img src="./assets/img/w8.png" alt="pg">',
+      pgW: '<img src="./assets/img/left.png" alt="pg">',
       baloonPixel: '<img src="./assets/img/baloon.png" alt="baloon">',
       isStartMove: false,
-      isMove: false
+      isMove: false,
+      fpsUno: null,
+      fpsDue: null,
     }
   },
 
   methods:{
-    action(){
-      pgW= '';
-
+    actionRight(){
+      console.log('ciao');
+      fpsUno = setInterval(() => {
+        this.pgW = '<img src="./assets/img/right.png" alt="pg">';
+      },1000);
+      setTimeout(() => {
+        // this.pgW = '<img src="./assets/img/w8.png" alt="pg">';
+        clearInterval(fpsUno);
+      }, 10000);
+      
+    },
+    actionLeft(){
+      
+      console.log('ciao2');
+      setTimeout(() => {
+        fpsDue = setInterval(() => {
+          this.pgW = '<img src="./assets/img/left.png" alt="pg">';
+        },1000);
+        setTimeout(() => {
+          this.pgW = '<img src="./assets/img/left.png" alt="pg">';
+          clearInterval(fpsDue);
+        }, 10000);
+      },500);
     }
   },
 }).mount('#app')
