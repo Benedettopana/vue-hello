e74b6c0218513101ed66edba159ae9e56cd7857b
Benedettopana <benedetto.pana@gmail.com>
2024-03-11 15:29:58 +0100

    [add]right repo

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const { createApp} = Vue;
+
+createApp({
+  data(){
+    return{
+      msg: 'Ciao Booleaners',
+      myImg: '',
+      immagine: '<img src="./assets/img/2151120916.jpg" alt="landscape">',
+      pgW: '<img src="./assets/img/w8.png" alt="landscape">',
+      isStartMove: false,
+    }
+  },
+
+  methods:{
+    action(){
+      pgW= '';
+
+    }
+  },
+}).mount('#app')
