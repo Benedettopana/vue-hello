8f3f1f8213713d2d970fd40bd097bf1d45cb974c
Benedettopana <benedetto.pana@gmail.com>
2024-03-11 15:34:50 +0100

    [add]msg

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ createApp({
     return{
       msg: 'Ciao Booleaners',
       myImg: '',
+      msgAnimation: 'Fai click sul personaggio!',
       immagine: '<img src="./assets/img/2151120916.jpg" alt="landscape">',
       pgW: '<img src="./assets/img/w8.png" alt="landscape">',
       isStartMove: false,
